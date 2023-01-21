cea7a405fca77e8496c1460648d18c224180f993
Jesús Cárceles <jesuscarclem30@gmail.com>
2023-01-21 10:50:49 +0100

    Removed @types/node and formatted auto-layout

--- src/composables/auto-layout.ts
+++ src/composables/auto-layout.ts
@@ -1,17 +1,17 @@
-const triggerLayout = () => {
-    const { mode, changeMode } = useLayoutControl()
-    const { matches } = window.matchMedia(`(max-width: ${import.meta.env.VITE_MAX_WIDTH_MOBILE}px)`)
-
-    if (matches && mode === "mobile" ||
-        !matches && mode === "desktop") {
-        return
+export const useTriggerLayout = () => {
+    
+    const triggerLayout = () => {
+        const { mode, changeMode } = useLayoutControl()
+        const { matches } = window.matchMedia(`(max-width: ${import.meta.env.VITE_MAX_WIDTH_MOBILE}px)`)
+    
+        if (matches && mode === "mobile" ||
+            !matches && mode === "desktop") {
+            return
+        }
+    
+        changeMode(matches ? "mobile" : "desktop")
     }
 
-    changeMode(matches ? "mobile" : "desktop")
-}
-
-
-export const useTriggerLayout = () => {
     onMounted(() => {
         triggerLayout();
         window.onresize = triggerLayout
