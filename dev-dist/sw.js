a1cbc505debe151c960e2463596fa1af8ab87032
Serroda <jesuscarclem30@gmail.com>
2023-09-13 23:56:56 +0200

    auto-import and sw updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o6msefo8tdo"
+    "revision": "0.4lqgm52eebo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
